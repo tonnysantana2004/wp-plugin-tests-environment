d56b9ee5f62f2b1eeb61398a8b29ffefc7c8da4e
Tonny Santana <tonnysantanasantos@gmail.com>
2026-04-05 07:26:07 -0300

    refactor: require the depencencies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+
 # 1. Roda o entrypoint original em background (copia arquivos, cria wp-config.php)
 docker-entrypoint.sh php-fpm &
 PID=$!
@@ -10,6 +11,8 @@ PID=$!
 while [ ! -f /var/www/html/wp-config.php ]; do
   sleep 1
 done
+
+bash /install-wp-tests.sh wordpress_test root 'wordpress' mysql latest
 
 # 3. Instala o WordPress só se ainda não foi instalado
 if ! wp core is-installed --allow-root; then
@@ -25,12 +28,15 @@ fi
 
 # Generate the plugin files
 wp scaffold plugin $PLUGIN_SLUG --allow-root --force
-
 cd wp-content/plugins/$PLUGIN_SLUG
 
-# Install composer dependencies
-composer require --dev phpunit/phpunit ^9.6
-composer install
+composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
+
+composer require --dev phpunit/phpunit:^9.6 --no-install
+composer require --dev yoast/phpunit-polyfills:^2.0 --no-install
+composer require --dev squizlabs/php_codesniffer:^3.7 --no-install
+composer require --dev wp-coding-standards/wpcs:^3.0 --no-install
+composer require --dev phpcompatibility/phpcompatibility-wp:^2.1 --no-install
 
 # 4. Traz o php-fpm pra frente
 wait $PID
